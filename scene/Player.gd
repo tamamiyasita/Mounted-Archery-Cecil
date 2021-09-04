6a125e45aba442fe379e76b36d5ce26664dbdacb
tamamiyasita <46100058+tamamiyasita@users.noreply.github.com>
2021-09-04 16:10:49 +0900

    cmt

--- scene/Player.gd
+++ scene/Player.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+onready var anime = $AnimationPlayer
 onready var bow :Sprite = $Cecil/Bow
 onready var arrow_hand :Sprite = $Cecil/Arrow_H
+onready var arrow_hand_base_position = arrow_hand.global_position
 
 var Arrow = preload('res://scene/Arrow.tscn')
+var arrow = null
 
 const base_speed = 40
 const max_speed = 800
@@ -12,34 +15,52 @@ export var arrow_speed = base_speed
 export var gravity = 200
 
 var charge_time = false
-
+var ready_arrow = false
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed('shot'):
-		charge_time = true
-	if event.is_action_released('shot'):
-#		yield(get_tree().create_timer(1.0), "timeout")
-		shoot()
-		charge_time = false
+	if is_instance_valid(arrow) and ready_arrow:
+		if event.is_action_pressed('shot'):
+			anime.play('default')
+			charge_time = true
+			
+		elif event.is_action_released('shot'):
+			arrow.global_transform = bow.global_transform
+			arrow.velocity = arrow.transform.x * arrow_speed
+			arrow.gravity = gravity
+	#		yield(get_tree().create_timer(1.0), "timeout")
+			shoot(arrow)
+			charge_time = false
+			ready_arrow = false
 
 
 
 
-func shoot() -> void:
-	var b = Arrow.instance()
-	get_parent().add_child(b)
-#	b.global_position = global_position
-	b.global_transform = bow.global_transform
-	b.velocity = b.transform.x * arrow_speed
-	b.gravity = gravity
-#	b.rotation = bow.rotation
+func shoot(b) -> void:
+	b.fly()
 
 func _physics_process(delta: float) -> void:
+	if !is_instance_valid(arrow):
+		anime.play('Draw_a_Bow')
+		yield(anime, "animation_finished" )
+		ready_arrow = true
+		arrow = Arrow.instance()
+		get_parent().add_child(arrow)
+		arrow._ready()
+
 	bow.global_rotation = get_local_mouse_position().angle()
 	arrow_hand.global_rotation = get_local_mouse_position().angle()
-	if charge_time:
-		arrow_speed += 5
-		if max_speed < arrow_speed:
-			arrow_speed = max_speed
-	else:
-		arrow_speed = base_speed
+
+	if is_instance_valid(arrow):
+		arrow.global_rotation = get_local_mouse_position().angle()
+		arrow_hand.global_rotation = get_local_mouse_position().angle()
+		
+		if charge_time:
+			arrow_speed += 5
+			if max_speed < arrow_speed:
+				arrow_speed = max_speed
+			else:
+				arrow_hand.global_position.x -= 0.07
+				arrow.global_position = arrow_hand.global_position
+		else:
+			arrow_speed = base_speed
+			arrow_hand.global_position = arrow_hand_base_position
--- script/Arrow.gd
+++ script/Arrow.gd
@@ -1,15 +1,25 @@
 extends Area2D
 
 # テスト用速度
-var velocity = Vector2(350, 0)
+var velocity = Vector2(0, 0)
 
-
+func _ready() -> void:
+	set_process(false)
+	
 func _process(delta: float) -> void:
 	velocity.y += gravity * delta
 	position += velocity * delta
 	rotation = velocity.angle()
 	
+func fly():
+	set_process(true)
 
+	
 
 func _on_Arrow_body_entered(body: Node) -> void:
+	queue_free()
+
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
